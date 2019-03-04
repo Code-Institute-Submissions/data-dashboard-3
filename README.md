1784face716fb9d40f6306e29c6b38143531a07c
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2019-03-04 12:19:40 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Welcome to Derek Dhammaloka's Milestone Project - Interactive Front
+End.  This is about the Data Dashboard.
+
---
+++ static/js/graph.js
@@ -0,0 +1,80 @@
+queue()
+
+    .defer(d3.csv, "data/petrol-2018.csv")
+    .await(makeGraphs);
+
+// Function to Make Graphs
+
+function makeGraphs(error, petrolData) {
+
+    // Define Variable
+    var ndx = crossfilter(petrolData);
+
+    // Parse Date
+    var parseDate = d3.time.format("%d/%m/%Y").parse;
+        petrolData.forEach(function (d) {
+            d.visit_date = parseDate(d.visit_date);
+            
+        });
+
+
+
+
+    // Show Graphs
+    
+    show_spend_by_company(ndx);
+    show_spend_by_month(ndx);
+    
+    
+    dc.renderAll();
+
+}
+
+// Show Spend By Company
+function show_spend_by_company(ndx) {
+
+    // Define Variables
+    
+    var dim = ndx.dimension(dc.pluck('company'));
+    var total_spend_by_company = dim.group().reduceSum(dc.pluck('spend'));
+
+    // Create Bar Chart
+
+    dc.barChart("#spend-by-company")
+        .width(350)
+        .height(250)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .dimension(dim)
+        .group(total_spend_by_group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Company")
+        .yAxis().ticks(20);
+
+}
+
+// Show Spend By Month
+function show_spend_by_month(ndx) {
+
+    // Define Variables
+    
+    var date_dim = ndx.dimension(dc.pluck('visit_date'));
+    var total_spend_by_month = dim.group().reduceSum(dc.pluck('spend'));
+    var minDate = date_dim.bottom(1)[0].visit_date;
+    var maxDate = date_dim.top(1)[0].visit_date;
+        
+    // Create Line Chart
+        
+    dc.lineChart("#spend-per-month")
+            .width(1000)
+            .height(300)
+            .margins({top: 10, right: 50, bottom: 30, left: 50})
+            .dimension(date_dim)
+            .group(total_spend_by_month)
+            .transitionDuration(500)
+            .x(d3.time.scale().domain([minDate, maxDate]))
+            .xAxisLabel("Month")
+            .yAxis().ticks(4);
+
+}
