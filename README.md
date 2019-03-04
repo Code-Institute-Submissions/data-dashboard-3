9adb4ed09402f1a3736a0de0b8d1db1b229bdb8c
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2019-03-04 17:23:56 +0000

    Updated petrol data

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ a particular year (e.g. 2018).
 
 Wireframes
 
+
+## Technologies
+
+Use of JavaScript to Access D3, Crossfilter, DC and Queue
+
+https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.min.js
+https://cdnjs.cloudflare.com/ajax/libs/crossfilter/1.3.12/crossfilter.min.js
+https://cdnjs.cloudflare.com/ajax/libs/dc/2.1.8/dc.js
+https://cdnjs.cloudflare.com/ajax/libs/queue-async/1.0.7/queue.min.js
--- static/js/graph.js
+++ static/js/graph.js
@@ -10,15 +10,7 @@ function makeGraphs(error, petrolData) {
     // Define Variable
     var ndx = crossfilter(petrolData);
 
-    // Parse Date
-    var parseDate = d3.time.format("%d/%m/%Y").parse;
-        petrolData.forEach(function (d) {
-            d.visit_date = parseDate(d.visit_date);
-            
-        });
-
-
-
+    
 
     // Show Graphs
     
@@ -36,7 +28,7 @@ function show_spend_by_company(ndx) {
     // Define Variables
     
     var dim = ndx.dimension(dc.pluck('company'));
-    var total_spend_by_company = dim.group().reduceSum(dc.pluck('spend'));
+    var total_spend_by_company = dim.group();
 
     // Create Bar Chart
 
@@ -45,7 +37,7 @@ function show_spend_by_company(ndx) {
         .height(250)
         .margins({top: 10, right: 50, bottom: 30, left: 50})
         .dimension(dim)
-        .group(total_spend_by_group)
+        .group(total_spend_by_company)
         .transitionDuration(500)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
@@ -60,7 +52,7 @@ function show_spend_by_month(ndx) {
     // Define Variables
     
     var date_dim = ndx.dimension(dc.pluck('visit_date'));
-    var total_spend_by_month = dim.group().reduceSum(dc.pluck('spend'));
+    var total_spend_by_month = date_dim.group();
     var minDate = date_dim.bottom(1)[0].visit_date;
     var maxDate = date_dim.top(1)[0].visit_date;
         
