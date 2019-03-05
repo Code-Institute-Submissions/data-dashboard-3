612b9b528e39278cb0c293bee51a8f90fca24eb4
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2019-03-05 11:55:37 +0000

    Updated JSON data

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,6 +1,6 @@
 queue()
 
-    .defer(d3.csv, "data/petrol-2018.csv")
+    .defer(d3.json, "data/petrol.json")
     .await(makeGraphs);
 
 // Function to Make Graphs
@@ -10,7 +10,11 @@ function makeGraphs(error, petrolData) {
     // Define Variable
     var ndx = crossfilter(petrolData);
 
-    
+     var parseDate = d3.time.format("%d/%m/%Y").parse;
+        petrolData.forEach(function (d) {
+            d.date = parseDate(d.date);
+            
+        });
 
     // Show Graphs
     
@@ -22,22 +26,17 @@ function makeGraphs(error, petrolData) {
 
 }
 
-// Show Spend By Company
 function show_spend_by_company(ndx) {
 
-    // Define Variables
-    
-    var dim = ndx.dimension(dc.pluck('company'));
-    var total_spend_by_company = dim.group();
-
-    // Create Bar Chart
+    var dim = ndx.dimension(dc.pluck("company"));
+    var group = dim.group();
 
     dc.barChart("#spend-by-company")
         .width(350)
         .height(250)
         .margins({top: 10, right: 50, bottom: 30, left: 50})
         .dimension(dim)
-        .group(total_spend_by_company)
+        .group(group)
         .transitionDuration(500)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
@@ -45,20 +44,19 @@ function show_spend_by_company(ndx) {
         .yAxis().ticks(20);
 
 }
-
 // Show Spend By Month
 function show_spend_by_month(ndx) {
 
     // Define Variables
     
-    var date_dim = ndx.dimension(dc.pluck('visit_date'));
+    var date_dim = ndx.dimension(dc.pluck('date'));
     var total_spend_by_month = date_dim.group();
-    var minDate = date_dim.bottom(1)[0].visit_date;
-    var maxDate = date_dim.top(1)[0].visit_date;
+    var minDate = date_dim.bottom(1)[0].date;
+    var maxDate = date_dim.top(1)[0].date;
         
     // Create Line Chart
         
-    dc.lineChart("#spend-per-month")
+    dc.lineChart("#spend-by-month")
             .width(1000)
             .height(300)
             .margins({top: 10, right: 50, bottom: 30, left: 50})
