ef99ed5d0957caaee453fd124fb4278ede428869
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2019-03-05 16:47:19 +0000

    Updated Index

--- static/js/graph.js
+++ static/js/graph.js
@@ -10,26 +10,26 @@ function makeGraphs(error, petrolData) {
     // Define Variable
     var ndx = crossfilter(petrolData);
 
-     var parseDate = d3.time.format("%d/%m/%Y").parse;
+
+    // Parse Date
+    var parseDate = d3.time.format("%d/%m/%Y").parse;
         petrolData.forEach(function (d) {
             d.date = parseDate(d.date);
-            
         });
 
     // Show Graphs
     
     show_spend_by_company(ndx);
     show_spend_by_month(ndx);
-    
-    
     dc.renderAll();
 
 }
 
+// Show Spend By Company
 function show_spend_by_company(ndx) {
 
     var dim = ndx.dimension(dc.pluck('company'));
-    var total_spend_by_company = dim.group();
+    var total_spend_by_company = dim.group().reduceSum(dc.pluck('spend'));
 
     dc.barChart("#spend-by-company")
         .width(350)
@@ -44,13 +44,14 @@ function show_spend_by_company(ndx) {
         .yAxis().ticks(20);
 
 }
+
 // Show Spend By Month
 function show_spend_by_month(ndx) {
 
     // Define Variables
     
     var date_dim = ndx.dimension(dc.pluck('date'));
-    var total_spend_by_month = date_dim.group();
+    var total_spend_by_month = date_dim.group().reduceSum(dc.pluck('spend'));
     var minDate = date_dim.bottom(1)[0].date;
     var maxDate = date_dim.top(1)[0].date;
         
